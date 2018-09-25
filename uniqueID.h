e82e30887e24a8317a4ffce5a31906e75ecf7da9
Tran Nhat Tuan <nhatuan87@yahoo.com>
2018-09-25 17:19:26 +0700

    rename function

--- uniqueID.h
+++ uniqueID.h
@@ -10,17 +10,25 @@
 
 const PROGMEM char P_date[]   = __DATE__  ;
 const PROGMEM char P_time[]   = __TIME__  ;
+uint16_t _crc16_update(uint16_t crc, uint8_t a) {
+    int i;
 
-uint16_t crc16_update(uint16_t& crc, const char* const p_str) {
+    crc ^= a;
+    for (i = 0; i < 8; ++i)
+    {
+        if (crc & 1)
+        crc = (crc >> 1) ^ 0xA001;
+        else
+        crc = (crc >> 1);
+    }
+
+    return crc;
+};
+
+uint16_t crc16_calc(uint16_t& crc, const char* const p_str) {
     for (unsigned int i = 0; i < strlen_P(p_str); i++) {
-        char c = pgm_read_byte_near(p_str + i);
-        crc ^= c;
-        for (int i = 0; i < 8; ++i) {
-            if (crc & 1)
-                crc = (crc >> 1) ^ 0xA001;
-            else
-                crc = (crc >> 1);
-        }
+        uint8_t a = pgm_read_byte_near(p_str + i);
+        crc = _crc16_update(crc, a);
     }
     return crc;
 }
@@ -30,8 +38,8 @@ uint16_t uniqueID() {
     return boot_signature_byte_get(0x16) << 8 |  boot_signature_byte_get(0x17);
 #else
     uint16_t crc = 0xFFFF;
-    crc16_update(crc, P_date);
-    crc16_update(crc, P_time);
+    crc16_calc(crc, P_date);
+    crc16_calc(crc, P_time);
     return crc;
 #endif
 }
